9bdca66e9321834b488ad31cfead3a52720b044d
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2025-03-16 21:25:58 +0530

    add user account routes for password change, current user retrieval, account updates, avatar and cover image uploads, channel profile, and watch history

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser,  loginUser, logoutUser, refreshAccessToken } from '../controllers/users.controller.js';
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccounDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from '../controllers/users.controller.js';
 import { upload } from '../middleware/multer.middleware.js';
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -22,7 +22,14 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //Secured routes
-router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route('/update-account').patch(verifyJWT, updateAccounDetails),
+    router.route('/avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar), //post method update whole field thats why used patch to update user avatar
+    router.route('/cover-image').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage)
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile),  //this path is used to pass params in url
+    router.route('/history').get(verifyJWT, getWatchHistory)
 
 export default router;
